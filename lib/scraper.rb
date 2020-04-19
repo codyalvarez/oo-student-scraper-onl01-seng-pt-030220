9129d10eba8f97f206093c492de3df875d8d064b
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-19 02:27:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,8 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-      html = open('fixtures/student-site/index.html')
-      doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
+      html = open(https://"learn-co-curriculum.github.io/student-scraper-test-page/")
+      doc = Nokogiri::HTML(html)
 
       scraped_students = []
 
