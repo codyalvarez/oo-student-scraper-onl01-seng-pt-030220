83aff31fadc596b4bb2cd69938a1f9686c7bc3f2
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-19 01:56:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,18 +4,19 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    student_hash = []
-    html = Nokogiri::HTML(open(index_url))
-    html.css(".student-card").collect do |student|
-      hash = {
-        name: student.css("h4.student-name").text,
-        location: student.css("p.student-location").text,
-        profile_url: "http://students.learn.co/" + student.css("a").attribute("href")
-      }
-      students_hash << hash
-    end
-    students_hash
-  end
+
+      students_hash = []
+      html = Nokogiri::HTML(open(index_url))
+      html.css(".student-card").collect do |student|
+        hash = {
+          name: student.css("h4.student-name").text,
+          location: student.css("p.student-location").text,
+          profile_url: "http://students.learn.co/" + student.css("a").attribute("href")
+        }
+        students_hash << hash
+      end
+      students_hash
+    end	  end
 
   def self.scrape_profile_page(profile_url)
 
