3ed6476573d404659a76c7b07ed98eb23665239a
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-19 01:55:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,14 +12,13 @@ class Scraper
         location: student.css("p.student-location").text,
         profile_url: "http://students.learn.co/" + student.css("a").attributes("href")
       }
-      students_hash << hash 
+      students_hash << hash
     end
     students_hash
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
