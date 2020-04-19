2e5c9db36221df0049d75bf1e230f71f42305d7c
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-19 01:56:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
       hash = {
         name: student.css("h4.student-name").text,
         location: student.css("p.student-location").text,
-        profile_url: "http://students.learn.co/" + student.css("a").attributes("href")
+        profile_url: "http://students.learn.co/" + student.css("a").attribute("href")
       }
       students_hash << hash
     end
